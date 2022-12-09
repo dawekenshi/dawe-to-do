c517db25588c7164d1ef0cd8cdc7d5100065fb7e
dawekenshi <104755414+dawekenshi@users.noreply.github.com>
2022-12-09 22:55:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const container = document.getElementById('mainer');
+const containerTwo = document.getElementById('gendiv');
+let defTitle;
+let defPara;
+let defDate;
+
+document.getElementById('add').addEventListener('click', (data) => {
+    defTitle = document.getElementById('title').value;
+    defPara = document.getElementById('more_info').value;
+    defDate = document.getElementById('deliver_time').value;
+})
+
+document.getElementById('add').addEventListener('click', (addDiv) =>{
+    const btnDlt = document.createElement('button');
+    btnDlt.textContent = "Delete";
+    btnDlt.setAttribute('id', 'sheesh');
+    btnDlt.addEventListener('click', (dereto) => {
+        newDiv.remove();
+    })
+    const newDiv = document.createElement('div');
+    const paragraph = document.createElement('p');
+    const title = document.createElement('h1');
+    const date = document.createElement('p');
+    const space = document.createElement('br');
+    paragraph.textContent = defPara;
+    title.textContent = defTitle;
+    date.textContent = defDate;
+    title.setAttribute('id', 'inner-title')
+    newDiv.setAttribute('id', 'gendiv');
+    newDiv.appendChild(title);
+    newDiv.appendChild(paragraph);
+    newDiv.appendChild(space);
+    newDiv.appendChild(date);
+    newDiv.appendChild(btnDlt);
+    container.appendChild(newDiv);
+    defTitle = document.getElementById('title').value = "";
+    defPara = document.getElementById('more_info').value = "";
+    defDate = document.getElementById('deliver_time').value = "";
+})
+
