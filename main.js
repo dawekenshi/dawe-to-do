50fe90c336eecf4214b780edb8064f4d13f99722
dawekenshi <104755414+dawekenshi@users.noreply.github.com>
2022-12-10 19:04:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,56 @@
+
+function saving(){
+    localStorage.setItem('save', JSON.stringify(elements));
+}
 const container = document.getElementById('mainer');
-const containerTwo = document.getElementById('gendiv');
+let elements = [];
+
+let sebedu = JSON.parse(localStorage.getItem('save'));
+
+document.addEventListener('DOMContentLoaded', (checkin) => {
+    let savedContent = JSON.parse(localStorage.getItem('save'));
+    for(let i = 0; i < savedContent.length; i++){
+        const newDiv = document.createElement('div');
+        const paragraph = document.createElement('p');
+        const title = document.createElement('h1');
+        const date = document.createElement('p');
+        const space = document.createElement('br');
+        paragraph.textContent = savedContent[i].paragraph;
+        title.textContent = savedContent[i].title;
+        date.textContent = savedContent[i].date;
+        elements.push(savedContent[i]);
+        title.setAttribute('id', 'inner-title')
+        newDiv.setAttribute('id', 'gendiv');
+        newDiv.appendChild(title);
+        newDiv.appendChild(paragraph);
+        newDiv.appendChild(space);
+        newDiv.appendChild(date);
+        container.appendChild(newDiv);
+        count++;
+    }
+})
+
 let defTitle;
 let defPara;
 let defDate;
+let count = 0;
+
+function saveToDo(title, paragraph, date){
+    this.title = title;
+    this.paragraph = paragraph;
+    this.date = date;
+}
+
 
 document.getElementById('add').addEventListener('click', (data) => {
     defTitle = document.getElementById('title').value;
     defPara = document.getElementById('more_info').value;
     defDate = document.getElementById('deliver_time').value;
 })
-
 document.getElementById('add').addEventListener('click', (addDiv) =>{
-    const btnDlt = document.createElement('button');
-    btnDlt.textContent = "Delete";
-    btnDlt.setAttribute('id', 'sheesh');
-    btnDlt.addEventListener('click', (dereto) => {
-        newDiv.remove();
-    })
+if(defDate == "" || defTitle == "" || defPara == ""){
+    alert("Please fill the needed info");
+}else{
     const newDiv = document.createElement('div');
     const paragraph = document.createElement('p');
     const title = document.createElement('h1');
@@ -25,20 +59,27 @@ document.getElementById('add').addEventListener('click', (addDiv) =>{
     paragraph.textContent = defPara;
     title.textContent = defTitle;
     date.textContent = defDate;
-    title.setAttribute('id', 'inner-title')
+    title.setAttribute('id', 'inner-title');    
     newDiv.setAttribute('id', 'gendiv');
-    if(defPara <= 0 || defDate <= 0 || defTitle <= 0){
-        alert("Please fill needed info");
-    }else{
-        newDiv.appendChild(title);
-        newDiv.appendChild(paragraph);
-        newDiv.appendChild(space);
-        newDiv.appendChild(date);
-        newDiv.appendChild(btnDlt);
-        container.appendChild(newDiv);
-        defTitle = document.getElementById('title').value = "";
-        defPara = document.getElementById('more_info').value = "";
-        defDate = document.getElementById('deliver_time').value = "";
-    }
+    let test = new saveToDo(defTitle, defPara, defDate);
+    elements.push(test);
+    newDiv.appendChild(title);
+    newDiv.appendChild(paragraph);
+    newDiv.appendChild(space);
+    newDiv.appendChild(date);
+    container.appendChild(newDiv);
+    defTitle = document.getElementById('title').value = "";
+    defPara = document.getElementById('more_info').value = "";
+    defDate = document.getElementById('deliver_time').value = "";
+    localStorage.clear();
+    count++;
+    
+}
+saving();
 })
 
+document.getElementById('dereto').addEventListener('click', (nashe) => {
+    localStorage.clear();
+    document.getElementById('gendiv').remove;
+    window.location.reload();
+})
